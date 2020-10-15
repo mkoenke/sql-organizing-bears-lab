d29819d693d5acf61529199e864a465e22ec9fd8
mkoenke <mkoenke@sas.upenn.edu>
2020-10-15 08:53:02 -0400

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 34, "M", "turquoise", "sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 15, "F", "blue", "rowdy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 6, "M", "purple", "sleepy", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 3, "F", "red", "disciplined", 0);
+INSERT INTO bears VALUES (5, "Melissa", 45, "F", "white", "arrogant", 1);
+INSERT INTO bears VALUES (6, "Grinch", 11, "M", "green", "envious", 0);
+INSERT INTO bears VALUES (7, "Wendy", 9, "F", "orange", "wishful", 1);
+INSERT INTO bears VALUES (8, null, 23, "M", "yellow", "angry", 0);
